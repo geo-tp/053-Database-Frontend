fedfb722feec0d05c0f9bdda9659690243250fca
geo <tp53.geo@gmail.com>
2022-10-01 04:07:51 +0200

    add map arrow func to display it or not

--- js/carousselSpawn.js
+++ js/carousselSpawn.js
@@ -37,3 +37,18 @@ function changeMapInCaroussel(event) {
     easterKingdomsContainer.style.display = "block";
   }
 }
+
+function determinateArrowToDisplay() {
+  const kalimdorContainer = document.querySelector("#map-container-Kalimdor");
+  const easterKingdomsContainer = document.querySelector(
+    "#map-container-Eastern_Kingdoms"
+  );
+
+  if (kalimdorContainer && easterKingdomsContainer) {
+    const mapArrowLeft = document.querySelector("#map-arrow-left");
+    const mapArrowRight = document.querySelector("#map-arrow-right");
+    mapArrowLeft.style.display = "block";
+    mapArrowRight.style.display = "block";
+  }
+}
+determinateArrowToDisplay();
